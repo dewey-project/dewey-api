7096bf20cac03341635341eb0b8c606d71bffd5c
jdmorlan <jay.d.morlan@gmail.com>
2016-04-19 00:14:57 -0500

    Add seeds to help with manual testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,18 +2,33 @@
 # with its default values. The data can then be loaded with the rake db:seed
 # (or created alongside the db with db:setup).
 
-school = School.create(title: 'Lonestar College')
+lonestar = School.create(title: 'Lonestar College')
+tech = School.create(title: 'Texas Tech')
 
-math = Department.create(title: 'Mathematics',
+lonestar_math = Department.create(title: 'Mathematics',
                          abbreviation: 'MATH',
-                         school: school)
+                         school: lonestar)
+
+tech_math = Department.create(title: 'Mathematics',
+                         abbreviation: 'MATH',
+                         school: tech)
 
 Course.create(title: 'College Algebra',
               code: 1314,
               credits: 3,
-              department: math)
+              department: lonestar_math)
 
 Course.create(title: 'Trigonometry',
               code: '1316',
               credits: 3,
-              department: math)
+              department: lonestar_math)
+
+Course.create(title: 'College Algebra',
+              code: 1314,
+              credits: 3,
+              department: tech_math)
+
+Course.create(title: 'Trigonometry',
+              code: '1316',
+              credits: 3,
+              department: tech_math)
