1bf2e654d65c120edeeed618d127b6876ecf6452
jdmorlan <jay.d.morlan@gmail.com>
2016-04-19 16:54:05 -0500

    Remove unneeded code

--- app/controllers/v1/courses_controller.rb
+++ app/controllers/v1/courses_controller.rb
@@ -6,10 +6,4 @@ class V1::CoursesController < ApplicationController
   def show
     @course = Course.includes(department: [:school]).find(params[:id])
   end
-
-  private
-
-  def allowed_params
-    params.require(:data).permit(:title, :code, :school_id, :department_id)
-  end
 end
